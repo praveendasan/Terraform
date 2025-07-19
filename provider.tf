24cb44f39eb9bb1397979a50c59e454991b4064f
Praveen Dasan <d.p.praveen@gmail.com>
2025-07-20 00:59:26 +0800

    resource created using code

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "940c7233-0ba0-43f7-98e4-4e2bf455d70d" // Add this line
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "my-tf-rg"
+location            = "East US"
