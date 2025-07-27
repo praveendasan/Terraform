29d8a4e06a23c124c3304926004fcb2a2abe1317
Praveen Dasan <d.p.praveen@gmail.com>
2025-07-27 19:08:04 +0800

    create initial resources to learn terraform state

---
+++ demo_state/main.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+region = "ap-southeast-1"
+}
+
+#create a s3 bucket to store terraform state
+resource "aws_s3_bucket" "terrraform_bucket_state" {
+  bucket = "terraform-up-and-running-state"
+  
+  #prevent deletion of the bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# Enable versioning for the S3 bucket
+resource "aws_s3_bucket_versioning" "s3_enable_versioning" {
+  bucket = aws_s3_bucket.terrraform_bucket_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable server-side encryption with AES-256
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt_default" {
+  bucket = aws_s3_bucket.terrraform_bucket_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  
+}
+
+#explicitly block public access to the bucket
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.terrraform_bucket_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
+
+#dynamodb table for state locking
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-up-and-running-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"   
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
+
